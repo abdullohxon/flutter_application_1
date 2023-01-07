deb29d6a80825be3b745cc95acb4ae8965bdba52
abdullohxon <122155347+abdullohxon@users.noreply.github.com>
2023-01-07 18:43:02 +0500

    --Abdulloh
    Matn qoshdim

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-//Abdulloh
+//Abdulloh Abduqodirov
   // This wlkdslfjsdlidget is the root of your application.
   @override
   Widget build(BuildContext context) {
